e48fb31f4398e47c138b959c82d56f9bee530f01
JOSH-akachukwu <onwuemenyijoshua@gmail.com>
2024-06-25 03:21:41 +0100

    edede

--- calc.py
+++ calc.py
@@ -1,2 +1,12 @@
-from  tkinter import Tk as x
-print(x)
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([0,2,4,6,8,10])
+y = np.array([0,3,5,7,9,11])
+
+plt.plot(x,y)
+
+plt.xlabel('age')
+plt.ylabel('name')
+
+plt.show()
