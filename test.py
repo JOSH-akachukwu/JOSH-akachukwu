733d965d2ce9d3a86e06be38b8752515270ce88a
JOSH-akachukwu <onwuemenyijoshua@gmail.com>
2024-07-09 22:36:45 +0100

    my world

--- test.py
+++ test.py
@@ -1,21 +1,14 @@
 ###just testing my python skills on random projects"
+
 import matplotlib.pyplot as plt
 import numpy as np
-import Django as d
-x = np.array([0,1,2,3,4,5])
-y = np.array([1,0,3,8,5,6])
 
-plt.subplot(2,1,1)
-plt.plot(x,y)
-
-x = np.array([0,1,2,3,4,5])
-y = np.array([1,0,3,8,5,6])
-plt.title("purchases")
-
-plt.subplot(2,1,2)
-plt.plot(x,y)
+x = np.array([1,3,4,5,6,5])
+y = np.array([2,4,5,2,5,6])
 plt.title("sales")
-
-plt.suptitle("Monthly reports")
+plt.grid(axis='x')
+plt.xlim()
+plt.ylim()
+plt.plot(x,y,"*--")
 plt.show()
 
--- trans.py
+++ trans.py
@@ -1,15 +1,21 @@
 print("Lets play a game \nin this game you are to pick a number perform some operations \nwith your choosen number and i will guess that number.")
-def trick():
+def game():
     import random
     x = random.randint(1,12)
     
     print("Pick a number in your mind. \nNow multiply it by 3. ")
     print("Now add", x, "to it" '\n' "then subtract your actual number from the result.")
-    
-    reslt = float(int(input("enter your final result :")))
-    number = (reslt - x)/2
-    print("Your number is",number)
-    return " "
+    try:
+        reslt = float(int(input("enter your final result :")))
+        number = (reslt - x)/2
+    except:
+        print("invalid result. Try playing with numbers")
+    else:
+        print("Your number is",number)
+    re_play = input("Press enter to play again.")
+    if re_play == '':
+        return game()
+    else:exit()
 
-x = trick()
+x = game()
 print(x)
